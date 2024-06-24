d89faa1f33d801a0f35d8599eb88dbd126366106
Zach Barnes <zachary.barnes.32@gmail.com>
2024-06-24 17:05:30 -0600

    starting with updating the template

--- app/controllers/TriviaController.js
+++ app/controllers/TriviaController.js
@@ -1,14 +1,30 @@
+import { AppState } from "../AppState.js"
 import { triviaService } from "../services/TriviaService.js"
+import { Pop } from "../utils/Pop.js"
+import { setHTML } from "../utils/Writer.js"
 
 export class TriviaController {
     constructor(){
         console.log('From the TC')
+        AppState.on('trivia', this.drawTrivia)
         this.getTrivia()
 
     }
 
-    getTrivia(){
-        console.log('getting trivia')
-        triviaService.getTrivia()
+    async getTrivia(){
+        try {
+            console.log('getting trivia')
+        await triviaService.getTrivia()
+        } catch (error) {
+            Pop.error(error)
+        }
+    }
+
+    drawTrivia(){
+        const trivia = AppState.trivia
+        let innerHTMLString = ''
+        trivia.forEach(trivia => innerHTMLString += trivia.triviaHTMLTemplate);
+        setHTML('trivia-questions', innerHTMLString)
+
     }
 }
--- app/models/Trivia.js
+++ app/models/Trivia.js
@@ -5,5 +5,21 @@ export class Trivia {
         this.incorrectAnswers = data.incorrect_answers
 
     }
+
+    get triviaHTMLTemplate(){
+        return `
+        <div>
+            <div class="card w-100">
+              <div class="card-body">
+                <h5 class="card-title">${this.question}</h5>
+                <div class="d-flex justify-content-around">
+                  <button>True</button>
+                  <button>False</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+    }
 }
 
